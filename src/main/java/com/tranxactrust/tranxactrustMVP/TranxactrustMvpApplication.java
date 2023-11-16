fd175f9ed48bb01d3e74091bc88ceda69a3b2618
devprojectteams <marvebirthday7@gmail.com>
2023-11-16 14:39:46 +0100

    first commit

---
+++ src/main/java/com/tranxactrust/tranxactrustMVP/TranxactrustMvpApplication.java
@@ -0,0 +1,13 @@
+package com.tranxactrust.tranxactrustMVP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranxactrustMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranxactrustMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tranxactrust/tranxactrustMVP/TranxactrustMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranxactrust.tranxactrustMVP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranxactrustMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
